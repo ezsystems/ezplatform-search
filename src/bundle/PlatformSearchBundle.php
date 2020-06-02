d2a925200b59411cbc716d962e2a0cc852ed7410
Dawid Parafiński <dawid.parafinski@ez.no>
2020-06-02 13:57:47 +0200

    EZP-31651: moved code

---
+++ src/bundle/PlatformSearchBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\Platform\Bundle\SearchBundle;
+
+use EzSystems\EzPlatformRestBundle\DependencyInjection\Compiler;
+use EzSystems\EzPlatformRestBundle\DependencyInjection\Security\RestSessionBasedFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PlatformSearchBundle extends Bundle
+{
+}
