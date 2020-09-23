b6e96350fb3f0a89eea2cfdeafb6a5c64280787b
Michał Grabowski <michal.grabowski@ez.no>
2020-09-23 13:09:12 +0200

    EZP-31929: design fixes

--- src/bundle/DependencyInjection/IbexaPlatformSearchExtension.php
+++ src/bundle/DependencyInjection/IbexaPlatformSearchExtension.php
@@ -10,14 +10,46 @@ namespace Ibexa\Platform\Bundle\Search\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class IbexaPlatformSearchExtension extends Extension
+class IbexaPlatformSearchExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependJMSTranslation($container);
+    }
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function prependJMSTranslation(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('jms_translation', [
+            'configs' => [
+                'ezplatform_search' => [
+                    'dirs' => [
+                        __DIR__ . '/../../',
+                    ],
+                    'output_dir' => __DIR__ . '/../Resources/translations/',
+                    'output_format' => 'xliff',
+                    'excluded_names' => ['*.module.js'],
+                    'excluded_dirs' => [],
+                    'extractors' => [],
+                ],
+            ],
+        ]);
     }
 }
