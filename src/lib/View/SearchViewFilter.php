5434b9e97595560c0c189b9102b7c9e1a2e924f2
Dawid Parafiński <dawid.parafinski@ez.no>
2021-03-05 13:34:34 +0100

    IBX-2: Specified action path for search form for easier reuse

--- src/lib/View/SearchViewFilter.php
+++ src/lib/View/SearchViewFilter.php
@@ -18,6 +18,7 @@ use Ibexa\Platform\Bundle\Search\Form\Type\SearchType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 
 class SearchViewFilter implements EventSubscriberInterface
 {
@@ -33,16 +34,21 @@ class SearchViewFilter implements EventSubscriberInterface
     /** @var \eZ\Publish\API\Repository\ContentTypeService */
     private $contentTypeService;
 
+    /** @var \Symfony\Component\Routing\RouterInterface */
+    private $router;
+
     public function __construct(
         ConfigResolverInterface $configResolver,
         FormFactoryInterface $formFactory,
         SectionService $sectionService,
-        ContentTypeService $contentTypeService
+        ContentTypeService $contentTypeService,
+        RouterInterface $router
     ) {
         $this->configResolver = $configResolver;
         $this->formFactory = $formFactory;
         $this->sectionService = $sectionService;
         $this->contentTypeService = $contentTypeService;
+        $this->router = $router;
     }
 
     public static function getSubscribedEvents()
@@ -110,6 +116,7 @@ class SearchViewFilter implements EventSubscriberInterface
             [
                 'method' => Request::METHOD_GET,
                 'csrf_protection' => false,
+                'action' => $this->router->generate('ezplatform.search'),
             ]
         );
 
